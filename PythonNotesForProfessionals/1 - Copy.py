649185e8f2d3551fbdc86b365d51eda83a458a7a
Yogesh Chhetri <yogeshkchhetri@gmail.com>
2021-05-05 14:56:35 +0530

    new message

---
+++ PythonNotesForProfessionals/1 - Copy.py
@@ -0,0 +1,18 @@
+print ('classic hello  world')
+v1 = 1
+v2 = 0
+v3 = 9.12
+v4 = 812876120938712
+v5 = 'ok now'
+
+print(v1)
+print(v2)
+print(v3)
+print(v4)
+print(v5)
+
+
+v = None #empty keyword
+print(v)
+b = True #boolean value
+print(b)
